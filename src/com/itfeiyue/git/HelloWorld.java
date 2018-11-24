15c10d212875526bcb5d5f606d9fa05b6423ebc3
liufengyun <liufengyun@163.com>
2018-11-24 23:51:04 +0800

    添加test工程至git

---
+++ src/com/itfeiyue/git/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.itfeiyue.git;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
